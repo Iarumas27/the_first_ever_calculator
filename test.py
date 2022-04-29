a43d635c6eaba9c649043ef60248c7d96725ebe4
iarum <iarumas27@gmail.com>
2022-04-29 21:19:19 +0700

    Add the first calculator

---
+++ test.py
@@ -0,0 +1,31 @@
+# My first ever calculator V2.0
+#
+from colorama import init
+from colorama import Fore, Back, Style
+
+
+def main():
+	# use Colorama to make Termcolor work on Windows too
+	init()
+
+	print(Back.BLUE)
+
+	what = input("What I have to do? (+, -): ")
+
+	a = float(input("Please enter the first number: "))
+	b = float(input("Please enter the second number: "))
+
+	print(Back.RED)
+
+	if what == "+":
+		c = a + b
+		print("Result: " + str(c))
+	elif what == "-":
+		c = a - b
+		print("Result: " + str(c))
+	else:
+		print("Selected wrong operation!")
+
+
+if __name__ == '__main__':
+    main()
